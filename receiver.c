7cf04f3ac79ebded12bd5d0eb38a2edbd30c4835
Pasic Haris <hpasic@julien.educ.insa>
2018-11-16 11:31:54 +0100

    Ca avance...

--- receiver.c
+++ receiver.c
@@ -9,15 +9,24 @@
 #include "./cacheutils.h"
 
 #define MIN_CACHE_MISS_CYCLES (285)
+#define SYNCING_CYCLE 1000000000.
+#define WORKING_CYCLE 100000000 
 
 size_t kpause = 0;
 
 int nb_hits = 0;
 int nb_miss = 0;
 int firstHit = 0; // bool
+int isSynced = 0; // bool
 size_t beginTime;
 char message[9];
 int cpt = 0;
+
+void reset() {
+  firstHit = 0;
+  nb_hits = 0;   
+  nb_miss = 0;  
+}
 
 void flushandreload(void* addr)
 {
@@ -25,19 +34,17 @@ void flushandreload(void* addr)
   maccess(addr);
   size_t delta = rdtsc() - time;
   flush(addr);
-  if (delta < MIN_CACHE_MISS_CYCLES)
-  {
-    if (kpause > 0)
-    {
+  if (delta < MIN_CACHE_MISS_CYCLES) {
+    if (kpause > 0) {
       
+      if(isSynced) {
        if(!firstHit) {
-         firstHit = 1;
+         reset();
          beginTime = rdtsc();
-         nb_hits = 0;   
-         nb_miss = 0;   
+         firstHit = 1;          
        }
-
-        nb_hits++;     
+       nb_hits++;     
+      }
 
       printf("%lu: Cache Hit (%lu cycles) after a pause of %lu cycles\n", time, delta, kpause);
     }
@@ -45,12 +52,56 @@ void flushandreload(void* addr)
   }
   else {
     kpause++;
-    nb_miss++;
+    if(isSynced) nb_miss++;
+  }  
+}
+
+long file_size(const char *filename) {
+   struct stat s; 
+   if (stat(filename,&s) != 0) {
+      printf("Error reading file stats !\n" );
+      return 0;
+   } 
+   return s.st_size; 
+}
+
+int main(int argc, char** argv) {
+  char* name = argv[1];
+  char* offsetp = argv[2];
+  if (argc != 3) {    
+    printf("usage: ./receiver file offset\n");
+    printf("example: ./receiver chaton.jpeg 0x00\n");
+    return 1;
   }
-  if(firstHit && rdtsc() - beginTime >= 10000000) {
-      double mean = nb_hits/(nb_miss*1.0);
+
+  unsigned int offset = 0;
+  !sscanf(offsetp,"%x",&offset);
+  int fd = open(name,O_RDONLY);
+
+  if (fd < 3) {
+    printf("error: failed to open file\n");
+    return 2;
+  }
+
+  unsigned char* addr = (unsigned char*) mmap(0, file_size(name), PROT_READ, MAP_SHARED, fd, 0);
+  if (addr == (void*)-1 || addr == (void*)0) {
+    printf("error: failed to mmap\n");
+    return 2;
+  }
+
+  while(1) {    
+
+    if(((int)(rdtsc() / SYNCING_CYCLE)) % 10 == 0)  {isSynced = 1; reset();}
+    else                                            isSynced = 0;    
+
+    flushandreload(addr + offset);
+
+    // printf("%d\n", isSynced);
+
+    if(isSynced && firstHit && rdtsc() - beginTime >= WORKING_CYCLE) {
+      double mean = nb_hits / ((double) nb_miss);
       printf("NbHits: %d, NbMiss: %d, Mean: %f \n", nb_hits, nb_miss, mean);
-      if(cpt < 8) {        
+      /*if(cpt < 8) {
         if(mean > 0.7) message[cpt] = 1;
         else message[cpt] = 0;
         cpt++;
@@ -58,63 +109,11 @@ void flushandreload(void* addr)
           message[cpt] = '\0';
           printf("%s", message);
         }
-      }      
-      firstHit = 0;
-      nb_hits = 0;   
-      nb_miss = 0;   
-  }
-}
-
-long file_size(const char *filename)
-{
-   struct stat s;
- 
-   if  (stat(filename,&s) != 0) {
-      printf("error!\n" );
-      return 0;
-   }
- 
-   return s.st_size;
-}
-
-int main(int argc, char** argv)
-{
-  char* name = argv[1];
-  char* offsetp = argv[2];
-  if (argc != 3)
-  {    
-    printf("  usage: ./receiver file offset\n"
-                 "example: ./receiver chaton.jpeg 0x00\n");
-    return 1;
-  }
-  unsigned int offset = 0;
-  !sscanf(offsetp,"%x",&offset);
-  int fd = open(name,O_RDONLY);
-  if (fd < 3)
-  {
-    printf("error: failed to open file\n");
-    return 2;
-  }
-
-  unsigned char* addr = (unsigned char*)mmap(0, file_size(name), PROT_READ, MAP_SHARED, fd, 0);
-  if (addr == (void*)-1 || addr == (void*)0)
-  {
-    printf("error: failed to mmap\n");
-    return 2;
-  }
-  
-  size_t time;
-  int r;
-
-  while(1)
-  {
-    time = rdtsc();    
-    r = time / 10000000000;
-    if(r % 10 == 0) {
-      flushandreload(addr + offset);
+      }*/
+      reset();  
     }    
-    for (int i = 0; i < 3; ++i)
-      sched_yield();
+
+    for (int i = 0; i < 3; ++i) sched_yield();
   }
   return 0;
 }
--- sender.c
+++ sender.c
@@ -9,30 +9,27 @@
 #include "./cacheutils.h"
 
 #define MIN_CACHE_MISS_CYCLES (285)
+#define SYNCING_CYCLE 1000000000.
+#define WORKING_CYCLE 100000000 
 
-long file_size(const char *filename)
-{
-   struct stat s;
- 
-   if  (stat(filename,&s) != 0) {
-      printf("error!\n" );
+long file_size(const char *filename) {
+   struct stat s; 
+   if (stat(filename,&s) != 0) {
+      printf("Error reading file stats !\n");
       return 0;
-   }
- 
-   return s.st_size;
+   } 
+   return s.st_size; 
 }
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
   char* name = argv[1];
   char* offsetp = argv[2];
   char* value = argv[3];
   char* length = argv[4];
 
-  if (argc != 5)
-  {    
-    printf("  usage: ./sender file offset value valueLength\n"
-                 "example: ./sender chaton.jpeg 0x00 100110 6\n");
+  if (argc != 5) {    
+    printf("usage: ./sender file offset value valueLength\n");
+    printf("example: ./sender chaton.jpeg 0x00 100110 6\n");
     return 1;
   }
 
@@ -43,36 +40,45 @@ int main(int argc, char** argv)
   !sscanf(length,"%d",&len);
 
   int fd = open(name,O_RDONLY);
-  if (fd < 3)
-  {
+  if (fd < 3) {
     printf("error: failed to open file\n");
     return 2;
   }
 
   unsigned char* addr = (unsigned char*) mmap(0, file_size(name), PROT_READ, MAP_SHARED, fd, 0);
 
-  if (addr == (void*)-1 || addr == (void*)0)
-  {
+  if (addr == (void*) -1 || addr == (void*) 0) {
     printf("error: failed to mmap\n");
     return 2;
   }
 
-  int r;
   size_t time;
+  int isSynced = 0;
+  int i = -1;
 
-  // infinite loop
-
-  for(int i = 0; i < len; ++i) {
+  while(1) {
     
-    do {
-      time = rdtsc();    
-      r = time / 10000000000;
-    }
-    while(r%10!=0);
+    // SYNC
+    if(((int)(rdtsc() / SYNCING_CYCLE)) % 10 == 0)  { 
+      isSynced = 1;
+      time = rdtsc();
 
-    if(value[i] == '1') while(rdtsc() - time < 10000000) maccess(addr + offset);
-    else                while(rdtsc() - time < 10000000);
+
+
+    // TODO  IF SYNC blabla
+      i++;
+    }
+    else isSynced = 0;
+
+
+    printf("%d, %d \n", isSynced, i);
+    
+    // COMMUNICATE
+    if(isSynced) {
+       if(i>20) goto end;
+       if(value[i] == '1')  if(rdtsc() - time < WORKING_CYCLE) maccess(addr + offset); // SEND 1
+       else                 if(rdtsc() - time < WORKING_CYCLE);                        // WAIT
+    }
   }
-  
-  return 0;
+  end: return 0;
 }
